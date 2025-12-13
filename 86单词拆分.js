bc57632aa60f5b314b51ed7b989ff09ced248b4b
何红妤#A05018 <hongyu.he@casstime.com>
2025-12-13 20:59:43 +0800

    单词拆分

--- 86单词拆分.js
+++ 86单词拆分.js
@@ -1,3 +1,17 @@
 var wordBreak = function (s, wordDict) {
+  const dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const str = s.slice(j, i);
+      if (wordDict.includes(str) && dp[j]) {
+        dp[i] = true;
+      }
+    }
+  }
+  return dp[dp.length - 1]
+}
 
-};
+const s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+const result = wordBreak(s, wordDict);
+console.log(result)
